380c0f8e712483d13cfcc72cb270e8f7e066f35c
BManon <37870473+BManon@users.noreply.github.com>
2019-07-25 11:27:00 +0200

    Deleted Zeitstempel - we understood it wrongly

--- src/app/supplier-registration/supplier-registration.component.ts
+++ src/app/supplier-registration/supplier-registration.component.ts
@@ -63,7 +63,6 @@ export class SupplierRegistrationComponent implements OnInit {
   abilities_t = [
     { id: 'PALETTENANSCHLAGLEISTE', ability: 'Palettenanschlagleiste vorhanden'},
     { id: 'HEBEBUHNE', ability: 'Hebebühne  vorhanden'},
-    { id: 'ZEITSTEMPEL', ability: 'Zeitstempel'},
   ];
 
  
